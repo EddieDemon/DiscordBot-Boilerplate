6f3954672e804de95e76d7f702ffa4bd3715a415
EddieDemon <934242+EddieDemon@users.noreply.github.com>
2022-02-04 21:24:02 +0100

    Add project files.

---
+++ DiscordBot Boilerplate/Interactions/ButtonModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+using Discord.Interactions;
+
+namespace DiscordBot_Boilerplate.Interactions
+{
+    public class ButtonModule : InteractionModuleBase<ShardedInteractionContext<SocketMessageComponent>>
+    {
+        [ComponentInteraction("btn:*")]
+        public async Task ButtonClicked(string whichButton)
+        {
+            var response = whichButton switch
+            {
+                "click" => "Yay, you clicked me!",
+                "dontclick" => "It said, DO NOT CLICK!!!",
+                _ => "Realy don't know what button this is."
+            };
+            await RespondAsync(response).ConfigureAwait(false);
+        }
+    }
+}
---
+++ DiscordBot Boilerplate/Interactions/SelectMenuModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.WebSocket;
+using Discord.Interactions;
+
+namespace DiscordBot_Boilerplate.Interactions
+{
+    public class SelectMenuModule : InteractionModuleBase<ShardedInteractionContext<SocketMessageComponent>>
+    {
+        [ComponentInteraction("select")]
+        public async Task ButtonClicked(string[] selectedValues)
+        {
+            await RespondAsync($"You've selected {string.Join(", ", selectedValues)}").ConfigureAwait(false);
+        }
+    }
+}
